7f25d1ee6673d8d03806d91ce4603c23dea35740
zhangchunlish <zhangchunlish@yeah.net>
2018-06-12 17:14:23 +0800

    register

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -1,32 +1,40 @@
 <template>
     <div>
-        <van-cell-group>
-            <van-field
-                v-model="username"
-                label="用户名"
-                icon="clear"
-                placeholder="请输入用户名"
-                required
-                @click-icon="username = ''"
-            />
-
-            <van-field
-                v-model="password"
-                type="password"
-                label="密码"
-                placeholder="请输入密码"
-                required
-            />
-        </van-cell-group>
+        <van-nav-bar
+        title="用户注册"
+        left-text="返回"
+        left-arrow
+        @click-left="goBack"
+        />
+        <div class="register-panel">
+        <van-field
+            v-model="username"
+            label="用户名"
+            icon="clear"
+            placeholder="请输入用户名"
+            required
+            @click-icon="username = ''"
+        />
+        <van-field
+            v-model="password"
+            type="password"
+            label="密码"
+            placeholder="请输入密码"
+            required
+        />
+        <div class="register-button">
+            <van-button type="primary" size="large">马上注册</van-button>
+        </div>
+       </div>
     </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style scoped>
-
+.register-panel{
+    
+}
 </style>
 
 
