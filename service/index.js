4abacb6f9dce8b7c1100a7a7c998a472b9c38696
zhangchunlish <zhangchunlish@yeah.net>
2018-06-13 13:22:12 +0800

    注册

--- service/index.js
+++ service/index.js
@@ -3,20 +3,40 @@ const app = new Koa()
 //引入connect
 const mongoose = require('mongoose')
 const {connect , initSchemas} = require('./database/init.js')
+//引入koa-bodyparser,获取前端请求
+const bodyParser = require('koa-bodyparser')
+//引入koa2-cors,解决跨域问题
+const cors = require('koa2-cors')
+//引入koa-router
+const Router = require('koa-router')
+
+app.use(bodyParser())
+app.use(cors())
+
+let user=require('./appApi/User.js')
+//装载所有子路由
+let router = new Router();
+router.use('/user',user.routes())
+
+//加载路由中间件
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
 //立即执行函数
-;(async () =>{
-    await connect()
-    initSchemas()
-    const User = mongoose.model('User')
-    let oneUser = new User({userName:'zcl',password:'123456'})
-    oneUser.save().then(()=>{
-        console.log('插入成功')
-    })
-    let  users = await  User.findOne({}).exec()
-    console.log('------------------')
-    console.log(users)
-    console.log('------------------')  
-})()
+// ;(async () =>{
+//     await connect()
+//     initSchemas()
+//     const User = mongoose.model('User')
+//     let oneUser = new User({userName:'zcl',password:'123456'})
+//     oneUser.save().then(()=>{
+//         console.log('插入成功')
+//     })
+//     let  users = await  User.findOne({}).exec()
+//     console.log('------------------')
+//     console.log(users)
+//     console.log('------------------')  
+// })()
 
 app.use(async(ctx)=>{
     ctx.body = '<h1>hello Koa2</h1>'
--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -7,33 +7,83 @@
         @click-left="goBack"
         />
         <div class="register-panel">
-        <van-field
-            v-model="username"
-            label="用户名"
-            icon="clear"
-            placeholder="请输入用户名"
-            required
-            @click-icon="username = ''"
-        />
-        <van-field
-            v-model="password"
-            type="password"
-            label="密码"
-            placeholder="请输入密码"
-            required
-        />
-        <div class="register-button">
-            <van-button type="primary" size="large">马上注册</van-button>
-        </div>
+            <van-field
+                v-model="username"
+                label="用户名"
+                icon="clear"
+                placeholder="请输入用户名"
+                required
+                @click-icon="username = ''"
+            />
+            <van-field
+                v-model="password"
+                type="password"
+                label="密码"
+                placeholder="请输入密码"
+                required
+            />
+            <div class="register-button">
+                <van-button type="primary" size="large" @click="axiosRegisterUser" :loading="openLoading">马上注册</van-button>
+            </div>
        </div>
     </div>
 </template>
 <script>
-export default {};
+import axios from "axios";
+import url from "@/serviceAPI.config.js";
+import { Toast } from "vant";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      openLoading: false //是否开启用户的Loading
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+    axiosRegisterUser() {
+      this.openLoading=true;
+      axios({
+        url: url.registerUser,
+        method: "post",
+        data: {
+          userName: this.username,
+          password: this.password
+        }
+      })
+        .then(response => {
+          console.log(response);
+          //如果返回code为200，代表注册成功，我们给用户作Toast提示
+          if (response.data.code == 200) {
+            Toast.success("注册成功");
+            this.$router.push("/");
+          } else {
+            console.log(response.data.message);
+            Toast.fail("注册失败");
+            this.openLoading=false;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          Toast.fail("注册失败");
+          this.openLoading=false
+        });
+    }
+  }
+};
 </script>
 <style scoped>
-.register-panel{
-    
+.register-panel {
+  width: 96%;
+  border-radius: 5px;
+  margin: 20px auto;
+  padding-bottom: 50px;
+}
+.register-button {
+  padding-top: 10px;
 }
 </style>
 
