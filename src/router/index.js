4416b9cbdf8babca4afe8661b0f7176b8184e971
zhangchunlish <zhangchunlish@yeah.net>
2018-08-07 15:37:22 +0800

    '底部导航'

--- src/router/index.js
+++ src/router/index.js
@@ -7,39 +7,43 @@ import Login from '@/components/pages/Login'
 import Goods from '@/components/pages/Goods'
 import CategoryList from '@/components/pages/CategoryList'
 import Cart from '@/components/pages/Cart'
+import Main from '@/components/pages/Main'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
+    path: "/main",
+    name: 'Main',
+    component: Main,
+    children: [{
       path: '/',
       name: 'ShoppingMall',
       component: ShoppingMall
-    },{
-      path: '/register',
-      name: 'Register',
-      component: Register
-    },{
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },{
-      path: '/Goods',
-      name: 'Goods',
-      component: Goods
-    },{
+    }, {
       path: '/CategoryList',
       name: 'CategoryList',
       component: CategoryList
-    },{
+    }, {
       path: '/Cart',
       name: 'Cart',
       component: Cart
-    },{
-      path: '/vuex',
-      name: 'Count',
-      component: Count
-    }
-  ]
+    }]
+  }, {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }, {
+    path: '/Goods',
+    name: 'Goods',
+    component: Goods
+  }, {
+    path: '/vuex',
+    name: 'Count',
+    component: Count
+  }]
 })
